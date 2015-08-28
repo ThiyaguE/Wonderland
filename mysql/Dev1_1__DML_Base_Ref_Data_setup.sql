b955fe5e3a6057294cb99ecafe529e7939a2cb96
Administrator <Administrator@scsbwin-535689.ITTPA.EBIZ.VERIZON.COM>
2015-08-28 01:16:28 -0700

    adding new line in 1.1

--- mysql/Dev1_1__DML_Base_Ref_Data_setup.sql
+++ mysql/Dev1_1__DML_Base_Ref_Data_setup.sql
@@ -1,3 +1,4 @@
 insert into olympian_info values (1, 'Aphrodite', 'The sensual goddess of Love and Beauty', 'Goddess');
 insert into olympian_info values (2, 'Apollo', 'The youthful god of the Sun and the Music', 'god');
 insert into olympian_info values (3, 'Ares', 'The fierce god of War', 'god');
+insert into olympian_info values (3, 'Ares', 'The fierce god of War', 'god');
