57b8a81602cd0b52f456882191bf33d5e12eac95
Administrator <Administrator@scsbwin-331982.ITTPA.EBIZ.VERIZON.COM>
2015-10-09 07:40:18 -0700

    Checking in Backout scripts.

---
+++ Backout-Demo-to-Balaji/Backout10151_2_1__DML_IR18927_Fix.sql
@@ -0,0 +1,3 @@
+insert into olympian_info values (8, 'Hades', 'The gloomy god of the Underworld');
+insert into olympian_info values (9, 'Hephaestus', 'The ill-favored god of Metallurgy');
+insert into olympian_info values (10, 'Hera', 'The mature goddess of the Family');
---
+++ Backout-Demo-to-Balaji/Backout10151_2__DML_West_Order_init.sql
@@ -0,0 +1,4 @@
+insert into olympian_info values (4, 'Artemis', 'The wild goddess of the Hunt');
+insert into olympian_info values (5, 'Athena', 'The sophisticated goddess of Wisdom and Arts');
+insert into olympian_info values (6, 'Demeter', 'The natural goddess of the Harvest');
+insert into olympian_info values (7, 'Dionysus', 'The joyful god of Wine');
